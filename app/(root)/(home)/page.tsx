cb3751c3454454eefc435f36ee8634416b87464e
Mukesh Kumar <mkmehta2041@gmail.com>
2023-10-30 18:25:50 +0530

    add auth

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div>
       <h1 className="h1-bold"> Next.js 13 we</h1>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
