de1fff13fe7417b775b3a11349c408308eb91747
Mukesh Kumar <mkmehta2041@gmail.com>
2023-10-31 18:29:54 +0530

    Add theme menubar

--- components/shared/navbar/NavBar.tsx
+++ components/shared/navbar/NavBar.tsx
@@ -2,10 +2,11 @@ import { SignedIn, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Theme from "./Theme";
 
 const Navbar = () => {
   return (
-    <nav className="flex-between background-light900_dark200 shadow-light-300 fixed z-50 w-full p-6 dark:shadow-none sm:px-12">
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full p-6 shadow-light-300 dark:shadow-none sm:px-12">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/assets/images/site-logo.svg"
@@ -21,6 +22,8 @@ const Navbar = () => {
       {/* GlobalSearch */}
 
       <div className="flex-between gap-5">
+        <Theme />
+
         <SignedIn>
           <UserButton
             afterSignOutUrl="/"
---
+++ constants/index.ts
@@ -0,0 +1,5 @@
+export const themes = [
+  { value: "light", label: "Light", icon: "/assets/icons/sun.svg" },
+  { value: "dark", label: "Dark", icon: "/assets/icons/moon.svg" },
+  { value: "System", label: "Light", icon: "/assets/icons/computer.svg" },
+];
