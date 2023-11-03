41beacbbb96ed51e14a14aa1606e25406bca01b1
Mukesh Kumar <mkmehta2041@gmail.com>
2023-11-03 17:50:11 +0530

    add home route

--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -4,7 +4,7 @@ import { Input } from "@/components/ui/input";
 
 const GlobalSearch = () => {
   return (
-    <div className="relative w-full max-w-[600px] max-lg:hidden">
+    <div className="relative w-full max-w-[900px] max-lg:hidden">
       <div className="background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-xl px-4 ">
         <Image
           src="/assets/icons/search.svg"
