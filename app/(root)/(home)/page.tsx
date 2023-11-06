245261d95fecfa325e3208d19759fc7163035ec6
mukesh kumar <mkmehta2041@gmail.com>
2023-11-06 16:19:56 +0530

    add Filter component

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,7 @@
+import Filter from "@/components/shared/Filter";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { Button } from "@/components/ui/button";
+import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
 
 export default function Home() {
@@ -22,7 +24,11 @@ export default function Home() {
           otherClasses="flex-1"
           placeholder="Search for questions.."
         />
-        Filters
+        <Filter
+          filters={HomePageFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          containerClasses="hidden max-md:flex"
+        />
       </div>
     </>
   );
