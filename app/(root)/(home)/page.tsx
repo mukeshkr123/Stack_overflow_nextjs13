2b106c560a01e37722800b477a33ad6e44042175
mukesh kumar <mkmehta2041@gmail.com>
2023-11-07 16:02:22 +0530

    add noresult component

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,65 @@
 import HomeFilter from "@/components/home/HomeFilter";
 import Filter from "@/components/shared/Filter";
+import NoResult from "@/components/shared/NoResult";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
+
+const questions = [
+  {
+    _id: 1,
+    title: "Cascadibg Delete in Sql",
+    tags: [
+      { _id: 1, title: "sql" },
+      { _id: 2, title: "python" },
+    ],
+    author: "Mukesh",
+    upvotes: 10,
+    viewsL: 100,
+    anwers: 2,
+    createAt: "2021-09-01T12:00:00.000Z",
+  },
+  {
+    _id: 2,
+    title: "Cascadibg Delete in Sql",
+    tags: [
+      { _id: 1, title: "sql" },
+      { _id: 2, title: "python" },
+    ],
+    author: "Mukesh",
+    upvotes: 10,
+    viewsL: 100,
+    anwers: 2,
+    createAt: "2021-09-01T12:00:00.000Z",
+  },
+  {
+    _id: 3,
+    title: "Cascadibg Delete in Sql",
+    tags: [
+      { _id: 1, title: "sql" },
+      { _id: 2, title: "python" },
+    ],
+    author: "Mukesh",
+    upvotes: 10,
+    viewsL: 100,
+    anwers: 2,
+    createAt: "2021-09-01T12:00:00.000Z",
+  },
+  {
+    _id: 4,
+    title: "Cascadibg Delete in Sql",
+    tags: [
+      { _id: 1, title: "sql" },
+      { _id: 2, title: "python" },
+    ],
+    author: "Mukesh",
+    upvotes: 10,
+    viewsL: 100,
+    anwers: 2,
+    createAt: "2021-09-01T12:00:00.000Z",
+  },
+];
 
 export default function Home() {
   return (
@@ -32,6 +88,23 @@ export default function Home() {
         />
       </div>
       <HomeFilter />
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {/* looping through questions  */}
+
+        {questions.length > 10 ? (
+          questions.map((question) => "QuestionCard")
+        ) : (
+          <NoResult
+            title=" There's no question to show"
+            description=" Be the first to break the silence! 🚀 Ask a Question and kickstart the
+          discussion. our query could be the next big thing others learn from. Get
+          involved! 💡"
+            link="/ask-question"
+            linkTitle=" Ask a Question"
+          />
+        )}
+      </div>
     </>
   );
 }
